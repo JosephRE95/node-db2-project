a8cbef6d6a256cadb0d9021b044458a9f5a9c102
Joseph Ellis <josephellis95@gmail.com>
2024-04-25 07:31:22 -1000

    table created

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,16 @@
-exports.up = function (knex) {
-  // DO YOUR MAGIC
+exports.up = async function(knex) {
+  await knex.schema.createTable('cars', table =>{
+    table.increments('cars_id')
+    table.string('vin', 20).unique().notNullable()
+    table.string('make').notNullable()
+    table.string('model').notNullable()
+    table.integer('mileage').notNullable()
+    table.string('title')
+    table.string('transmission')
+  })
 };
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('cars')
 };
